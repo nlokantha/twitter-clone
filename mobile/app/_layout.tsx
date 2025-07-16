fd6e3669bc2a69b7cf60f6d5b8d83c4e5dedcf10
nlokantha <nlokantha@gmail.com>
2025-07-16 23:15:25 +0530

    Add user synchronization and API client functionality
    - Implemented `useUserSync` hook for syncing user data with the server.
    - Created API client with token handling for authenticated requests.
    - Integrated `@tanstack/react-query` for managing server state.
    - Updated dependencies in `package.json` and `package-lock.json`.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,12 +2,21 @@ import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from "expo-router";
 import "../global.css"
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
   return <ClerkProvider tokenCache={tokenCache}>
+    <QueryClientProvider client={queryClient}>
     <Stack>
       <Stack.Screen name="(auth)" options={{headerShown:false}}/>
       <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
     </Stack>
+    </QueryClientProvider>
   </ClerkProvider>
 }
